be62fd6a8d77f23b8b513ea4f4b4fa0153b9950e
marikar <marius.karotkis@nksc.lt>
2026-09-16 13:59:54 +0300

    Adding Lithuanian localization

---
+++ src/locales/lt/common.js
@@ -0,0 +1,12 @@
+const TRANSLATIONS = {
+  chat: {
+    "send-message": "Siųsti žinutę",
+    "reset-chat": "Iš naujo nustatyti pokalbį",
+    "close-chat": "Uždaryti",
+    "message-copied": "Nukopijuota!",
+    "session-id": "Sesijos ID",
+    "email-support": "Pagalba el. paštu",
+  },
+};
+
+export default TRANSLATIONS;
